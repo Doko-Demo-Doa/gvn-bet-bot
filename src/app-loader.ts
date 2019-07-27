80cdbfdbd9476c874c3467e96cf603c33392e6c3
Quan Pham <doraemonfanclub@gmail.com>
2019-07-27 23:35:17 +0700

    Remove linting

--- src/app-loader.ts
+++ src/app-loader.ts
@@ -1,19 +1,16 @@
-const axios = require('axios')
 const Discord = require('discord.js')
-const client = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+const Commando = require('discord.js-commando')
+const client = new Commando.Client({
+  owner: process.env.OWNER,
+  commandPrefix: 'gvn'
 })
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
-  }
-
-  axios.get('https://api.github.com/users/github')
-    .then(r => console.log(r.data))
-    .catch(e => console.log(e))
-})
+/**
+ * All Discord events are listed here:
+ */
+client
+  .on('ready', () => console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`))
+  .on('message', (msg: any) => console.log(msg))
 
 client.login(process.env.DISCORD_TOKEN)
