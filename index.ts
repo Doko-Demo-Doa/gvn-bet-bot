bf30a841f051ba987330d519e77afed541d04cbe
Quan Pham <doraemonfanclub@gmail.com>
2019-08-05 18:08:14 +0700

    Removed redundant code

--- index.ts
+++ index.ts
@@ -3,14 +3,10 @@ require("reflect-metadata");
 require("./src/app-loader");
 
 import { createConnection } from "typeorm";
-import { DiscordUser } from "./src/entities/user";
 
 createConnection()
   .then(async connection => {
     await connection.query('PRAGMA foreign_keys=OFF');
-    // await connection.synchronize();
-
-    const firstUser = await DiscordUser.find({ take: 2 });
-    console.log(firstUser);
+    await connection.synchronize();
   })
   .catch(error => console.log(error));
