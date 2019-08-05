0f46ea59d58c397d51596b8a91fdb4e2a275b682
Quan Pham <doraemonfanclub@gmail.com>
2019-08-05 17:34:36 +0700

    Linked to Nadeko

--- index.ts
+++ index.ts
@@ -3,10 +3,14 @@ require("reflect-metadata");
 require("./src/app-loader");
 
 import { createConnection } from "typeorm";
+import { DiscordUser } from "./src/entities/user";
 
 createConnection()
   .then(async connection => {
     await connection.query('PRAGMA foreign_keys=OFF');
-    await connection.synchronize();
+    // await connection.synchronize();
+
+    const firstUser = await DiscordUser.find({ take: 2 });
+    console.log(firstUser);
   })
   .catch(error => console.log(error));
