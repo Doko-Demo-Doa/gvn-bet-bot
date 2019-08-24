f883df03430d1a11363a2dc04d14fc71557a9469
Quan Pham <doraemonfanclub@gmail.com>
2019-08-25 02:30:45 +0700

    Added code cleanup

--- src/entities/bet-log.ts
+++ src/entities/bet-log.ts
@@ -3,24 +3,21 @@ import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 /**
  * Actual "bet" session.
  */
-@Entity({ name: "DiscordBet" })
-export class DiscordBet extends BaseEntity {
+@Entity({ name: "DiscordBetLog" })
+export class DiscordBetLog extends BaseEntity {
   @PrimaryGeneratedColumn({ name: "Id" })
   id: number;
-
-  @Column({ name: "Amount" })
-  amount: number;
 
   /**
    * Prediction of the match:
    * 1: Team 1 wins.
    * 2: Team 2 wins.
    */
-  @Column({ name: "Prediction" })
-  prediction: number;
+  @Column({ name: "ActionType" })
+  actionType: number; // 0 = đặt kèo, 1 = change team.
 
-  @Column({ name: "DateAdded" })
-  dateAdded: string;
+  @Column({ name: "RecordDate" })
+  recordDate: number; // Unix timestamp.
 
   @Column({ name: "UserId" })
   userId: string;
